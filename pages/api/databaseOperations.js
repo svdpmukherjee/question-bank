67d6ddbec4f50598776702b0f33a6a31255a7246
svdpmukherjee <100602182+svdpmukherjee@users.noreply.github.com>
2022-05-10 17:45:22 +0200

    Update databaseOperations.js

--- pages/api/databaseOperations.js
+++ pages/api/databaseOperations.js
@@ -32,7 +32,7 @@ async function updateEntry(req, res) {
       {
         $inc: {
           // answer: (parseFloat(answer.toFixed(3)) + 0.001).toString(),
-          answer: 0.001,
+          answer: 1,
         },
       }
     );
